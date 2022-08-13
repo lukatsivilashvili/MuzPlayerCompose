53e657213541119a0d8ffb267a9891145726231f
Luka Tsivilashvili <lukawivilashvili@gmail.com>
2022-08-13 13:03:24 +0400

    Fix Notification Default Icon

--- app/src/main/java/com/example/muzplayer/exoplayer/MusicNotificationManger.kt
+++ app/src/main/java/com/example/muzplayer/exoplayer/MusicNotificationManger.kt
@@ -7,25 +7,30 @@ import android.app.PendingIntent
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Color
-import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Build
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.MediaSessionCompat
+import androidx.annotation.RequiresApi
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.transition.Transition
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.muzplayer.R
+import com.example.muzplayer.extensions.checkHasArt
 import com.example.muzplayer.utils.Constants.NOTIFICATION_ID
+import com.example.muzplayer.utils.Constants.NOTIFICATION_LARGE_ICON_SIZE
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import kotlinx.coroutines.*
 
 class MusicNotificationManger(
     private val context: Context,
     sessionToken: MediaSessionCompat.Token,
     notificationListener: PlayerNotificationManager.NotificationListener,
-    private val newSongCallback: () -> Unit
 ) {
 
     private val notificationManager: PlayerNotificationManager
+    private val serviceJob = SupervisorJob()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
 
     init {
         val channelId =
@@ -47,40 +52,61 @@ class MusicNotificationManger(
         notificationManager.setPlayer(player)
     }
 
-    private inner class DescriptionAdapter(
-        private val mediaController: MediaControllerCompat
-    ) : PlayerNotificationManager.MediaDescriptionAdapter {
+    private inner class DescriptionAdapter(private val controller: MediaControllerCompat) :
+        PlayerNotificationManager.MediaDescriptionAdapter {
 
-        override fun getCurrentContentTitle(player: Player): CharSequence {
-            newSongCallback()
-            return mediaController.metadata.description.title.toString()
-        }
+        var currentIconUri: Uri? = null
+        var currentBitmap: Bitmap? = null
 
-        override fun createCurrentContentIntent(player: Player): PendingIntent? {
-            return mediaController.sessionActivity
-        }
+        override fun createCurrentContentIntent(player: Player): PendingIntent? =
+            controller.sessionActivity
 
-        override fun getCurrentContentText(player: Player): CharSequence {
-            return mediaController.metadata.description.subtitle.toString()
-        }
+        override fun getCurrentContentText(player: Player) =
+            controller.metadata.description.subtitle.toString()
 
+        override fun getCurrentContentTitle(player: Player) =
+            controller.metadata.description.title.toString()
+
+        @RequiresApi(Build.VERSION_CODES.Q)
         override fun getCurrentLargeIcon(
             player: Player,
             callback: PlayerNotificationManager.BitmapCallback
         ): Bitmap? {
-            Glide.with(context).asBitmap()
-                .load(mediaController.metadata.description.iconUri)
-                .into(object : CustomTarget<Bitmap>() {
-                    override fun onResourceReady(
-                        resource: Bitmap,
-                        transition: Transition<in Bitmap>?
-                    ) {
-                        callback.onBitmap(resource)
-                    }
+            val iconUri = getArt()
+            return if (currentIconUri != iconUri || currentBitmap == null) {
+                // Cache the bitmap for the current song so that successive calls to
+                // `getCurrentLargeIcon` don't cause the bitmap to be recreated.
+                currentIconUri = iconUri
+                serviceScope.launch {
+                    currentBitmap = resolveUriAsBitmap(iconUri)
+                    currentBitmap?.let { callback.onBitmap(it) }
+                }
+                null
+            } else {
+                currentBitmap
+            }
+        }
 
-                    override fun onLoadCleared(placeholder: Drawable?) = Unit
-                })
-            return null
+        private suspend fun resolveUriAsBitmap(uri: Uri?): Bitmap? {
+            val toLoad = uri ?: R.drawable.ic_default_artwork
+            return withContext(Dispatchers.IO) {
+                // Block on downloading artwork.
+                Glide.with(context)
+                    .asBitmap()
+                    .load(toLoad)
+                    .diskCacheStrategy(DiskCacheStrategy.DATA)
+                    .submit(NOTIFICATION_LARGE_ICON_SIZE, NOTIFICATION_LARGE_ICON_SIZE)
+                    .get()
+            }
+        }
+
+        @RequiresApi(Build.VERSION_CODES.Q)
+        private fun getArt(): Uri?{
+            return if(controller.metadata.description.mediaUri.checkHasArt(context)){
+                controller.metadata.description.iconUri
+            }else{
+                null
+            }
         }
     }
     private fun createNotificationChannel(channelId: String, channelName: String): String{
