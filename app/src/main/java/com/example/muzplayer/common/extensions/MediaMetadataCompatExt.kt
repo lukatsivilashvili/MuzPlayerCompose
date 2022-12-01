ebcab6ecd07f642d38244be8e6061490d8bca3d3
Luka Tsivilashvili <lukawivilashvili@gmail.com>
2022-12-01 20:18:37 +0400

    Add CrashLytics SDK and update libraries

--- app/src/main/java/com/example/muzplayer/common/extensions/MediaMetadataCompatExt.kt
+++ app/src/main/java/com/example/muzplayer/common/extensions/MediaMetadataCompatExt.kt
@@ -3,15 +3,13 @@ package com.example.muzplayer.common.extensions
 import android.support.v4.media.MediaMetadataCompat
 import com.example.muzplayer.domain.models.Song
 
-fun MediaMetadataCompat.toSong(): Song? {
-    return description?.let {
-        Song(
-            mediaId = it.mediaId ?: "",
-            title = it.title.toString(),
-            subtitle = it.subtitle.toString(),
-            duration = it.description.toString().toLong(),
-            songUrl = it.mediaUri.toString(),
-            imageUrl = it.iconUri.toString()
-        )
-    }
+fun MediaMetadataCompat.toSong(): Song {
+    return Song(
+        mediaId = description.mediaId ?: "",
+        title = description.title.toString(),
+        subtitle = description.subtitle.toString(),
+        duration = description.description.toString().toLong(),
+        songUrl = description.mediaUri.toString(),
+        imageUrl = description.iconUri.toString()
+    )
 }
